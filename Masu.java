f1f809c8900fa286365a63c3f956f02c5911ac85
Kae-Ng <75514206+Kae-Ng@users.noreply.github.com>
2021-01-03 14:48:34 +0900

    Update Masu.java

--- Masu.java
+++ Masu.java
@@ -1,13 +1,15 @@
-public class Masu {
+public class Masu implements Initializable {
     public static final int TYPE_VOID = 0;
     public static final int TYPE_SHIP = 1;
 
     private int[][] map;
     private int width;
     private int height;
-    private int av;  //count if ship is there
+    private int availablility;  //count if ship is there
+    private int ships;
 
     Masu (int width, int height) {
-
+        this.width = width;
+        this.height = height;
     }
 }
