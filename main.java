0e170b880773d600e3c43db7896250db7cfa4030
Kae-Ng <75514206+Kae-Ng@users.noreply.github.com>
2021-01-03 15:18:25 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 class main implements Initializable {
   public Masu map;
-  public Battleship[4] ships;
+  public Battleship[] ships;
 
+  public static final int[][] startPos = {{0,0},
+                                          {0,4},
+                                          {4,0},
+                                          {4,4}};
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-    map = new Masu(5, 5);
-    ships = new Battelship();
+    mapThis = new Masu(5, 5);
+    mapOppo = new Masu(5, 5);
+    myShips = new Battleship[4];
+    theirShips = new Battleship[4];
+    for (int i = 0; i<4; i++) {
+      myShips[i].setPos(startPos[i]);
+    }
   }
 
   static public void main (String arg[]) {
