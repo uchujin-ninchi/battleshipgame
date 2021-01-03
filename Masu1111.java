3d98304c9eb99c66151be5b0b4973b12e5a13e8c
ariaria-a <76079944+ariaria-a@users.noreply.github.com>
2021-01-03 17:06:45 +0900

    Update Masu1111.java

--- Masu1111.java
+++ Masu1111.java
@@ -6,6 +6,7 @@ public class MasuData implements Initializable {
     private int width;
     private int height;
     private int ships;
+    private int Av;
 
     MasuData (int width, int height) {
         this.width =  width;
@@ -19,48 +20,14 @@ public class MasuData implements Initializable {
     public int getShipdata(int y, int x){
         return  map[y][x];
     }
-    public int getMap(int x, int y) {
-        if (x < 0 || width <= x || y < 0 || height <= y) {
-            return -1;
-        }
-        return maps[y][x];
+    public int getAv(int y, int x){
+        return map[y][x];
     }
-    public void setMap(int x, int y, int type){
-        if (x < 1 || width <= x-1 || y < 1 || height <= y-1) {
-            return;
-        }
-        maps[y][x] = type;
-    }
-    public void fillMap(int type){
-        for (int y=0; y<height; y++){
-            for (int x=0; x<width; x++){
-                maps[y][x] = type;
-            }
-        }
-    }
-
-    public int getHeight(){
-        return height;
-    }
-    public int getWidth(){
-        return width;
-    }
-
-}
-public class AvData implements Initializable {
-    public static final int TYPE_VOID = 0;
-    public static final int TYPE_SHIP = 1;
-
-    private int[][] map;
-    private int width;
-    private int height;
-    private int av;  //count if ship is there
-
-    AvData (int width, int height) {
-        this.width =  width;
-        this.height = height;
-        map = new Int[width][height];
-        fillMap(AvData.TYPE_VOID);
+    public void setAv(int y, int x){
+        this.y = width;
+        this.x = heigth;
+        map = new Int[y][x];
+        fillMap(MasuData.TYPE_VOID);
     }
     public int getMap(int x, int y) {
         if (x < 0 || width <= x || y < 0 || height <= y) {
