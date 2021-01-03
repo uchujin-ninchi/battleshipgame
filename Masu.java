6b4846716bdf02b2e30806941d05c36ca23d36b8
Kae-Ng <75514206+Kae-Ng@users.noreply.github.com>
2021-01-03 14:21:51 +0900

    Add files via upload

---
+++ Masu.java
@@ -0,0 +1,13 @@
+public class Masu {
+    public static final int TYPE_VOID = 0;
+    public static final int TYPE_SHIP = 1;
+
+    private int[][] map;
+    private int width;
+    private int height;
+    private int av;  //count if ship is there
+
+    Masu (int width, int height) {
+
+    }
+}
