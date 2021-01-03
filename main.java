7b5a066a4fb6922deb909f44806cf239fa1b6f90
Kae-Ng <75514206+Kae-Ng@users.noreply.github.com>
2021-01-03 14:45:37 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,16 @@
-import java.util.Scanner
+import java.util.Scanner;
+class main implements Initializable {
+  public Masu map;
+  public Battleship[4] ships;
+
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    map = new Masu(5, 5);
+    ships = new Battelship();
+  }
+
+  static public void main (String arg[]) {
+
+  }
+}
