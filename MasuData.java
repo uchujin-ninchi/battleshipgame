9c471048338c0953bea11abdcba286ad38c22d4f
Kae-Ng <75514206+Kae-Ng@users.noreply.github.com>
2021-01-03 17:32:25 +0900

    Update and rename Masu1111.java to MasuData.java

---
+++ MasuData.java
@@ -0,0 +1,57 @@
+public class MasuData implements Initializable {
+    public static final int TYPE_VOID = 0;
+    public static final int TYPE_SHIP = 1;
+
+    private int[][] map;
+    private int width;
+    private int height;
+    private int ships;
+    private int[][] AvMap;
+
+    MasuData (int width, int height) {
+        this.width = width;
+        this.height = height;
+        map = new int[width][height];
+        AvMap = new int[width][height];
+        fillShipData(MasuData.TYPE_VOID);
+        fillAvMap(MasuData.TYPE_VOID);
+    }
+
+    public int getMap(int x, int y) {
+        return maps[y][x];
+    }
+    public void setMap(int x, int y, int type){
+        map[y][x] = type;
+    }
+
+    public int getAv(int x, int y) {
+        return AvMap[y][x];
+    }
+    public void setAv(int x, int y, int count){
+        AvMap[y][x] = count;
+    }
+    
+    public void fillShipData(int type){
+        for (int y=0; y<height; y++){
+            for (int x=0; x<width; x++){
+                map[y][x] = type;
+            }
+        }
+    }
+
+    public void fillAvMap(int type){
+        for (int y=0; y<height; y++){
+            for (int x=0; x<width; x++){
+                map[y][x] = type;
+            }
+        }
+    }
+
+    public int getHeight(){
+        return height;
+    }
+    public int getWidth(){
+        return width;
+    }
+
+}
